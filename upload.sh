c5ddbe7ed9efbf7d9d37b6800a10803f7d11c23c
Josh Tari <josh.tari@dogmacompany.com>
2017-06-01 14:20:05 +0200

    [Feature] add libtari game skeleton

---
+++ upload.sh
@@ -0,0 +1,1 @@
+sudo ./dc-tool_mirk -t 192.168.0.4 -c filesystem -x 1ST_READ.BIN
---
+++ windows/post_build.bat
@@ -0,0 +1,4 @@
+copy /Y ..\..\addons\libtari\windows\vs15\DLL\* Release
+xcopy /E /Y /I ..\assets\fonts Release\fonts
+xcopy /E /I /Y ..\assets Release\assets
+xcopy /E /Y /I ..\assets\debug Release\debug
